b3d3dc8bb2adadbf0545cd20d76ccdfc70aa0af0
Tim <tuan.mrbean@gmail.com>
2018-05-21 14:42:33 +0700

    import nha thuoc

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,8 +6,39 @@ use \PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
 class HomeController extends BaseController {
   public function index($request, $response) {
-    $filePath = $this->baseDir() . 'resource/data.xlsx';
-    $data = $this->readExcel($filePath);
+    $filePath = $this->baseDir() . 'resource/Nha_thuoc_chuan_hoa_new.xlsx';
+    if(!isset($_SESSION['data_nhathuoc'])) {
+      $data = $this->readExcel($filePath);
+      $_SESSION['data_nhathuoc'] = $data;
+    } else {
+      $data = $_SESSION['data_nhathuoc'];
+    }
+    //echo count($data);
+    
+    echo "<pre>";
+    //print_r($data[2]);
+    //Database instance
+    $db = $this->db;
+
+    $validDataArr = [];
+    
+    foreach($data as $product) {
+      $validDataArr[] = array(
+        "store_id" => $product['A'],
+        "name" => $product['B'],
+        "address" => $product['C'],
+        "owner" => $product['D'],
+        "phone" => $product['E'],
+        "district" => $product['F'],
+        "province" => $product['G'],
+      );
+    }
+  
+    $this->db->insert('nha_thuoc', $validDataArr);
+    
+
+
+    die('Done import');
 
     $response->getBody()->write(json_encode($data));
     return $response->withHeader('Content-type', 'application/json');
